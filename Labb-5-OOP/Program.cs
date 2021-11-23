aa678eaf15f8ba549eb46418be53226501651143
LucasNarfgren <narfgren@hotmail.com>
2021-11-23 10:36:06 +0100

    Add project files.

---
+++ Labb-5-OOP/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Labb_5_OOP
+{
+    class Check<T>
+    {
+        public bool toCheck(T n1, T n2)
+        {
+            if (n1.Equals(n2))
+            {
+                Console.WriteLine("True");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("False");
+                return false;
+            }
+        }
+    }
+    class Check1 : Check<int>
+    {
+
+    }
+
+    class Check2 : Check<string>
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Check1 c1 = new Check1();
+            c1.toCheck(5,5);
+
+            Check2 c2 = new Check2();
+            c2.toCheck("Då","Hej");
+
+            Check1 c3 = new Check1();
+            c3.toCheck(10,5);
+
+            Check2 c4 = new Check2();
+            c4.toCheck("Luva","Luva");
+        }
+    }
+}
